c9f0e3cb396d1f26990a15c96a9fc2ea0fe2c182
Vigneshwaran <86647561+Vignesh2828@users.noreply.github.com>
2024-03-15 14:35:16 +0530

    unwanted imports removed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import ActionGrid from '@/components/card/card';
-import HorizontalLinearStepper from '@/components/card/card';
-import ActionAreaCard from '@/components/card/card';
-import GridBoxTest from '@/components/card/gridBoxTest';
 import MarkCalculator from '@/components/useFormHook_task/markcalculator';
 export default function Home() {
   return (
